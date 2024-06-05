a821e19a761984e73b08f8fe0c006d52074006f3
Jannatin Nurrohmah <61218518+Jannatin12@users.noreply.github.com>
2024-06-05 10:18:04 +0700

    Update DriverBelanjaan.java

--- src/main/java/DriverBelanjaan.java
+++ src/main/java/DriverBelanjaan.java
@@ -25,5 +25,21 @@ public class DriverBelanjaan {
         belanjaan.tampilkanItem();
 
         System.out.println("Total harga setelah reset: " + belanjaan.dapatkanTotalHarga());
+
+        belanjaan.tambahItem("Buku", 10000);
+        belanjaan.tambahItem("Pensil", 2000);
+        belanjaan.tambahItem("Penghapus", 1500);
+        
+        belanjaan.tampilkanItem();
+
+        System.out.println("Total harga: " + belanjaan.dapatkanTotalHarga() + "\n");
+
+        belanjaan.hapusItem("Pensil");
+        
+        belanjaan.tampilkanItem();
+
+        System.out.println("Total harga setelah pensil dihapus: " + belanjaan.dapatkanTotalHarga()  + "\n"  );
+
+        belanjaan.reset();
     }
 }
